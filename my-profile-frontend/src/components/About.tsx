973a3aec0908f2b7c3018598a8ea3e1047279da2
Tiantian Li <tiantiancscs@gmail.com>
2024-10-21 11:46:42 +1100

    Update About.tsx

--- my-profile-frontend/src/components/About.tsx
+++ my-profile-frontend/src/components/About.tsx
@@ -5,7 +5,7 @@ import { ContentContainer, Heading, Text } from '../styles/StyledComponents';
 const About: React.FC = () => (
   <ContentContainer>
     <Heading>About Me</Heading>
-    <Text>Highly motivated Full-Stack developer with two years of experience in designing, developing, and maintaining web applications. Proficient in both frontend and backend technologies, with a strong focus on creating efficient and scalable solutions. A fast learner who is passionate about teamwork and adheres to the SOLID principles to ensure robust and maintainable code.</Text>
+    <Text>Highly motivated Full-Stack developer with two years of experience in designing, developing, and maintaining web applications. Proficient in both frontend and backend technologies, with a strong focus on creating efficient and scalable solutions. A fast learner who is passionate about teamwork and adheres to the SOLID principles to ensure robust and maintainable code. Demonstrates exceptional problem-solving abilities, supported by research experience and published works, showcasing the capacity to tackle complex challenges and innovate effectively.</Text>
     <Heading>Languages</Heading>
     <div><strong>Mandarin:</strong> Native</div>
     <div><strong>English:</strong> Professional</div>
