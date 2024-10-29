f81789960b611eea98b088f9fed1c21396510ac4
Tiantian Li <tiantiancscs@gmail.com>
2024-10-29 23:12:09 +1100

    Update schema.sql

--- my-profile-backend-serverless/src/main/resources/mysql/schema.sql
+++ my-profile-backend-serverless/src/main/resources/mysql/schema.sql
@@ -1,25 +1,21 @@
 DROP TABLE IF EXISTS experience;
 CREATE TABLE experience (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    company_name VARCHAR(255) NOT NULL,
-    job_title VARCHAR(255) NOT NULL,
-    date_range VARCHAR(255) NOT NULL,
-    description TEXT,
-    location VARCHAR(255)
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        company_name VARCHAR(255) NOT NULL,
+        job_title VARCHAR(255) NOT NULL,
+        date_range VARCHAR(255) NOT NULL,
+        description JSON,
+        location VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS project;
 CREATE TABLE project (
-     id INT AUTO_INCREMENT PRIMARY KEY,
-     name VARCHAR(255) NOT NULL,
-     description VARCHAR(255),
-     detail TEXT,
-     link VARCHAR(255)
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(255),
+        detail JSON,
+        tag JSON,
+        git_url VARCHAR(255),
+        preview_url VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS skill;
-CREATE TABLE skill (
-     id INT AUTO_INCREMENT PRIMARY KEY,
-     category VARCHAR(255) NOT NULL,
-     items TEXT
-);
