a4de9aba74ff92bf04ff283946a392b464260918
Tiantian Li <tiantiancscs@gmail.com>
2024-11-07 17:10:37 +1100

    Update Footer.jsx

--- frontend-nextjs/src/app/components/Footer.jsx
+++ frontend-nextjs/src/app/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <span>
-          <img src="logo.svg" alt="Logo" className="h-10 w-10 bg-transparent rounded-full" />
+          <Image src="logo.svg" alt="Logo" className="h-10 w-10 bg-transparent rounded-full" />
         </span>
         <p className="text-slate-600">&copy; 2024 My Portfolio. All rights reserved.</p>
       </div>
