fe0aca0fd5007518aa5a02bc751f4465d945a20f
Tiantian Li <tiantiancscs@gmail.com>
2024-11-08 23:05:48 +1100

    Update HeroSection.jsx

--- frontend-nextjs/src/app/components/HeroSection.jsx
+++ frontend-nextjs/src/app/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -62,8 +62,8 @@ const HeroSection = () => {
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] overflow-hidden relative">
-            <Image
-              src="/images/profile/hero-image.png"
+            <img
+              src="/images/hero-image.png"
               alt="hero image"
               // className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               className="absolute w-full h-full object-cover top-0 left-0"
