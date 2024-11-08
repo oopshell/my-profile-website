4d4bb4cbd0e3dfcf2a02619f83c94c3779ff839e
Tiantian Li <tiantiancscs@gmail.com>
2024-11-08 23:05:47 +1100

    Update AboutSection.jsx

--- frontend-nextjs/src/app/components/AboutSection.jsx
+++ frontend-nextjs/src/app/components/AboutSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useTransition, useState } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import TabButton from "./TabButton";
 
 const TAB_DATA = [
@@ -65,7 +65,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/profile/about-image.png" width={500} height={1200} alt="About Image" />
+        <img src="/images/about-image.png" width={500} height={1200} alt="About Image" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
