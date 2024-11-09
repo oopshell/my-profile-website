ef2dc618976acb50d8a73dfd0f77b2dda06fde77
Tiantian Li <tiantiancscs@gmail.com>
2024-11-09 21:45:25 +1100

    Update AboutSection.jsx

--- frontend-nextjs/src/app/components/AboutSection.jsx
+++ frontend-nextjs/src/app/components/AboutSection.jsx
@@ -65,7 +65,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <img src="/images/about-image.png" width={500} height={1200} alt="About Image" />
+        <img src="/images/about-image.png" width={500} height={500} alt="About Image" className="fixed top-0 left-0" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
