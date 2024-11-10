bd89a41f7600dd982c664385cc4df45d6845c9e2
Tiantian Li <tiantiancscs@gmail.com>
2024-11-11 09:44:40 +1100

    Update ProjectDetailRepository.java

--- backend-serverless/src/main/java/tiantian_li/me/repository/ProjectDetailRepository.java
+++ backend-serverless/src/main/java/tiantian_li/me/repository/ProjectDetailRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 
 @Repository
 public interface ProjectDetailRepository extends JpaRepository<ProjectDetail, Long> {
-    // Custom method to find all details for a specific project
     List<ProjectDetail> findByProjectId(Long projectId);
 }
