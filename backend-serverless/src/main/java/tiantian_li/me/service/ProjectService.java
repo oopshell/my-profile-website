7006b6a7dbdcd8dd4f1d586dd8b8d29c87492b51
Tiantian Li <tiantiancscs@gmail.com>
2024-11-13 10:29:12 +1100

    Update ProjectService.java

--- backend-serverless/src/main/java/tiantian_li/me/service/ProjectService.java
+++ backend-serverless/src/main/java/tiantian_li/me/service/ProjectService.java
@@ -1,17 +1,25 @@
 package tiantian_li.me.service;
 
 import tiantian_li.me.entity.Project;
+import tiantian_li.me.entity.ProjectDetail;
+import tiantian_li.me.entity.ProjectTag;
 import java.util.List;
 
 public interface ProjectService {
 
-            Project saveProject(Project project);
+    Project saveProject(Project project);
 
-            List<Project> fetchAllProjects();
+    List<Project> fetchAllProjects();
 
-            Project getProjectById(Long id);
+    Project getProjectById(Long id);
 
-            Project updateProjectById(Long id, Project project);
+    Project getProjectBySlug(String slug);
 
-            String deleteProjectById(Long id);
+    Project updateProjectById(Long id, Project project, List<ProjectDetail> details, List<ProjectTag> tags);
+
+    String deleteProjectById(Long id);
+
+    Project addDetailsToProject(Long projectId, List<ProjectDetail> details);
+
+    Project addTagsToProject(Long projectId, List<ProjectTag> tags);
 }
