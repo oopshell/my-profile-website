cd88f44cb53ef78d48e8d0f2575ba3758ece009e
Tiantian Li <tiantiancscs@gmail.com>
2024-11-21 12:12:16 +1100

    Update ProjectTag.java

--- backend-serverless/src/main/java/tiantian_li/me/entity/ProjectTag.java
+++ backend-serverless/src/main/java/tiantian_li/me/entity/ProjectTag.java
@@ -1,14 +1,25 @@
 package tiantian_li.me.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "project_tag")
 public class ProjectTag {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
     private Long tagId;
 
     @Column(name = "tag_name", unique = true)
@@ -16,42 +27,7 @@ public class ProjectTag {
 
     // Many-to-many relationship with Project through ProjectTagMapping
     @ManyToMany(mappedBy = "tags")
+    @JsonIgnore
     private List<Project> projects;
 
-    // Constructors
-    public ProjectTag() {}
-
-    public ProjectTag(String tagName) {
-        this.tagName = tagName;
-    }
-
-    public ProjectTag(Long tagId, String tagName) {
-        this.tagId = tagId;
-        this.tagName = tagName;
-    }
-
-    // Getters and setters
-    public Long getTagId() {
-        return tagId;
-    }
-
-    public void setTagId(Long tagId) {
-        this.tagId = tagId;
-    }
-
-    public String getTagName() {
-        return tagName;
-    }
-
-    public void setTagName(String tagName) {
-        this.tagName = tagName;
-    }
-
-    public List<Project> getProjects() {
-        return projects;
-    }
-
-    public void setProjects(List<Project> projects) {
-        this.projects = projects;
-    }
 }
