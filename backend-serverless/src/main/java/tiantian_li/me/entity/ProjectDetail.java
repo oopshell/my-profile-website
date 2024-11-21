0ec6e22fc3f9b9d51d0b15c7bee1784198a998ab
Tiantian Li <tiantiancscs@gmail.com>
2024-11-21 12:12:13 +1100

    Update ProjectDetail.java

--- backend-serverless/src/main/java/tiantian_li/me/entity/ProjectDetail.java
+++ backend-serverless/src/main/java/tiantian_li/me/entity/ProjectDetail.java
@@ -1,18 +1,24 @@
 package tiantian_li.me.entity;
 
 import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "project_detail")
 public class ProjectDetail {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "detail_id")
     private Long detailId;
-
-    @ManyToOne
-    @JoinColumn(name = "project_id", nullable = false)
-    private Project project;
 
     @Column(name = "title")
     private String title;
@@ -21,52 +27,9 @@ public class ProjectDetail {
     @Column(name = "description")
     private String description;
 
-    // Constructors
-    public ProjectDetail() {}
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false, referencedColumnName = "project_id")
+    @JsonIgnore
+    private Project project;
 
-    public ProjectDetail(Project project, String title, String description) {
-        this.project = project;
-        this.title = title;
-        this.description = description;
-    }
-
-    public ProjectDetail(Long detailId, String title, String description, Project project) {
-        this.detailId = detailId;
-        this.title = title;
-        this.description = description;
-        this.project = project;
-    }
-
-    // Getters and setters
-    public Long getDetailId() {
-        return detailId;
-    }
-
-    public void setDetailId(Long detailId) {
-        this.detailId = detailId;
-    }
-
-    public Project getProject() {
-        return project;
-    }
-
-    public void setProject(Project project) {
-        this.project = project;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
