7ed39f980dce751778f7806c1bb9f2010e411dae
Tiantian Li <tiantiancscs@gmail.com>
2024-11-22 14:31:13 +1100

    Update Responsibility.java

--- backend-serverless/src/main/java/tiantian_li/me/entity/Responsibility.java
+++ backend-serverless/src/main/java/tiantian_li/me/entity/Responsibility.java
@@ -1,18 +1,24 @@
 package tiantian_li.me.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "responsibility")
 public class Responsibility {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "responsibility_id")
     private Long responsibilityId;
-
-    @ManyToOne
-    @JoinColumn(name = "experience_id", nullable = false)
-    private Experience experience;
 
     @Column(name = "title")
     private String title;
@@ -21,23 +27,9 @@ public class Responsibility {
     @Column(name = "description", length = 1000)
     private String description;
 
-    // Constructors
-    public Responsibility() {}
+    @ManyToOne
+    @JoinColumn(name = "experience_id", nullable = false, referencedColumnName = "experience_id")
+    @JsonIgnore
+    private Experience experience;
 
-    public Responsibility(Long responsibilityId, Experience experience, String title, String description) {
-        this.responsibilityId = responsibilityId;
-        this.experience = experience;
-        this.title = title;
-        this.description = description;
-    }
-
-    // Getters and setters
-    public Long getResponsibilityId() { return responsibilityId; }
-    public Experience getExperience() { return experience; }
-    public String getTitle() { return title; }
-    public String getDescription() { return description; }
-
-    public void setExperience(Experience experience) { this.experience = experience; }
-    public void setTitle(String title) { this.title = title; }
-    public void setDescription(String description) { this.description = description; }
 }
