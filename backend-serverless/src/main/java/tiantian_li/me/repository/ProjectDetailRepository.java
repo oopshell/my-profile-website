5e16c4fe473677f43e7882d40c7a796689ba53e6
Tiantian Li <tiantiancscs@gmail.com>
2024-11-22 14:31:20 +1100

    Update ProjectDetailRepository.java

--- backend-serverless/src/main/java/tiantian_li/me/repository/ProjectDetailRepository.java
+++ backend-serverless/src/main/java/tiantian_li/me/repository/ProjectDetailRepository.java
@@ -2,11 +2,17 @@ package tiantian_li.me.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tiantian_li.me.entity.ProjectDetail;
 
 import java.util.List;
 
 @Repository
 public interface ProjectDetailRepository extends JpaRepository<ProjectDetail, Long> {
-    List<ProjectDetail> findByProjectId(Long projectId);
+    @Query("SELECT d FROM ProjectDetail d WHERE d.project.id = :projectId")
+    List<ProjectDetail> findDetailsByProjectId(@Param("projectId") Long projectId);
+
+    @Query("SELECT d FROM ProjectDetail d WHERE d.project.slug = :slug")
+    List<ProjectDetail> findDetailsByProjectSlug(@Param("slug") String slug);
 }
