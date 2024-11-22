01df61ddef835dd89bf35cec3ce7ec08779bee63
Tiantian Li <tiantiancscs@gmail.com>
2024-11-22 14:31:14 +1100

    Update Skill.java

--- backend-serverless/src/main/java/tiantian_li/me/entity/Skill.java
+++ backend-serverless/src/main/java/tiantian_li/me/entity/Skill.java
@@ -1,54 +1,31 @@
 package tiantian_li.me.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "skill")
 public class Skill {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "skill_id")
+    private Long skillId;
 
     @Column(name = "skill_name", nullable = false)
     private String skillName;
 
     @ManyToOne
-    @JoinColumn(name = "category_id", nullable = false)
+    @JoinColumn(name = "category_id", nullable = false, referencedColumnName = "category_id")
+    @JsonIgnore
     private SkillCategory category;
 
-    // Constructors
-    public Skill() {
-    }
-
-    public Skill(Long id, String skillName, SkillCategory category) {
-        this.id = id;
-        this.skillName = skillName;
-        this.category = category;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSkillName() {
-        return skillName;
-    }
-
-    public void setSkillName(String skillName) {
-        this.skillName = skillName;
-    }
-
-    public SkillCategory getCategory() {
-        return category;
-    }
-
-    public void setCategory(SkillCategory category) {
-        this.category = category;
-    }
 }
