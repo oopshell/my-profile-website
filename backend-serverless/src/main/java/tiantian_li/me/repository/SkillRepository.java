784efba17cf0721d21f125d0d3732f1af985d3b8
Tiantian Li <tiantiancscs@gmail.com>
2024-11-27 10:56:49 +1100

    Update SkillRepository.java

--- backend-serverless/src/main/java/tiantian_li/me/repository/SkillRepository.java
+++ backend-serverless/src/main/java/tiantian_li/me/repository/SkillRepository.java
@@ -1,9 +1,16 @@
 package tiantian_li.me.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tiantian_li.me.entity.Skill;
 
+import java.util.List;
+
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Long> {
+    // Find all skills by category name
+    @Query("SELECT s FROM Skill s JOIN s.category c WHERE c.categoryName = :categoryName")
+    List<Skill> findSkillsByCategoryName(@Param("categoryName") String categoryName);
 }
