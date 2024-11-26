3c170ddf662f7fbc85b71ff435d7be6c0a459d51
Tiantian Li <tiantiancscs@gmail.com>
2024-11-27 10:56:46 +1100

    Update ResponsibilityRepository.java

--- backend-serverless/src/main/java/tiantian_li/me/repository/ResponsibilityRepository.java
+++ backend-serverless/src/main/java/tiantian_li/me/repository/ResponsibilityRepository.java
@@ -1,9 +1,15 @@
 package tiantian_li.me.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tiantian_li.me.entity.Responsibility;
 
+import java.util.List;
+
 @Repository
 public interface ResponsibilityRepository extends JpaRepository<Responsibility, Long> {
+    @Query("SELECT r FROM Responsibility r JOIN r.experience e WHERE e.experienceId = :experienceId")
+    List<Responsibility> findResponsibilitiesByExperienceId(@Param("experienceId") Long experienceId);
 }
