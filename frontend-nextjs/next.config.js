8bc488e3dc3d53b4e4f1dc013fe4075487937e21
Tiantian <tiantiancscs@gmail.com>
2025-02-08 15:50:44 +1100

    Update next.config.js

--- frontend-nextjs/next.config.js
+++ frontend-nextjs/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'my-profile-projects.s3.ap-southeast-2.amazonaws.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
- module.exports = nextConfig
+module.exports = nextConfig
