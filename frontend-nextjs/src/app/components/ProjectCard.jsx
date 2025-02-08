0824b105a2b96d80981e43a85bb19d1207ba33f5
Tiantian <tiantiancscs@gmail.com>
2025-02-08 12:45:45 +1100

    Update ProjectCard.jsx

--- frontend-nextjs/src/app/components/ProjectCard.jsx
+++ frontend-nextjs/src/app/components/ProjectCard.jsx
@@ -35,6 +35,7 @@ const ProjectCard = ({ slug, imgUrl, title, description, gitUrl, previewUrl }) =
           )}
           <Link
             href={`/project/${slug}`}
+            prefetch={false}
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <InformationCircleIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
