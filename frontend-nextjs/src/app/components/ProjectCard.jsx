3cafed6eb424851b7cb4876c42a2bc32139141ea
Tiantian <tiantiancscs@gmail.com>
2025-02-08 14:46:50 +1100

    Update ProjectCard.jsx

--- frontend-nextjs/src/app/components/ProjectCard.jsx
+++ frontend-nextjs/src/app/components/ProjectCard.jsx
@@ -4,16 +4,17 @@ import Link from "next/link";
 
 const ProjectCard = ({ slug, imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
-    <div className="w-full h-full bg-[#181818] rounded-xl overflow-hidden">
+    <div>
       <div
-        className="h-52 md:h-72 relative group"
+        className="h-52 md:h-72 rounded-t-xl relative group"
         style={{
           background: `url(${imgUrl})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
+          overflow: "hidden",
         }}
       >
-        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           {gitUrl && (
             <Link
               href={gitUrl}
@@ -41,9 +42,9 @@ const ProjectCard = ({ slug, imgUrl, title, description, gitUrl, previewUrl }) =
           </Link>
         </div>
       </div>
-      <div className="text-white p-4">
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
-        <p className="text-[#ADB7BE] text-sm line-clamp-2">{description}</p>
+        <p className="text-[#ADB7BE]">{description}</p>
       </div>
     </div>
   );
